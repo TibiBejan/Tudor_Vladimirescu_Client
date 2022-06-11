556945d4c3ea01b84ac3c35aef1538dd639b39a4
TibiBejan <tiberiu.bejan98@gmail.com>
2022-06-11 21:50:18 +0300

    using full path to api in requests

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
         // INIT REQ
         dispatch(requestCheckLogin);
 
-        const response = await axios.request({method: 'GET', url: "https://campus-tudor-2022.herokuapp.com/api/v1/auth/login", headers: {withCredentials: true}});
+        const response = await axios.request({method: 'GET', url: "https://campus-tudor-2022.herokuapp.com/api/v1/auth/login"});
         if(response.status === 200 || response.status === 201) {
           dispatch(receiveCheckLogin(response.data.data));
         } else {
